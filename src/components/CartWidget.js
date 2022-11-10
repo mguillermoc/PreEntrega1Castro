0fd3880fb2afc396cfb7c5a08419462cd2b5da44
Martin Castro <mguillermocastro@gmail.com>
2022-11-09 23:23:13 -0300

    Primer commit

---
+++ src/components/CartWidget.js
@@ -0,0 +1,13 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
+
+function CartWidget(){
+    return(
+  
+        <div className="cart1" id="nv4">
+        <FontAwesomeIcon icon={faCartShopping} />
+        </div>
+
+    )
+}
+export default CartWidget;
