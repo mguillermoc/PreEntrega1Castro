e786e4b2fde7c56c9683832eda301c6f8ae9ff59
Martin Castro <mguillermocastro@gmail.com>
2022-11-23 23:08:30 -0300

    PreEntrega2-Castro

---
+++ src/views/category.js
@@ -0,0 +1,11 @@
+import Navbar from "./components/navbar";
+
+
+export const CategoryView =() => {
+    return <div>
+    <Navbar/>
+        <h1>Categoria</h1>
+    </div>
+}
+
+export default CategoryView
